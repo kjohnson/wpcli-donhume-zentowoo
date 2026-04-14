723ac8e2b114a8365bbdd46f1579ed9c6e875a82
Kyle B. Johnson <kyle.johnson@hey.com>
2026-04-14 10:13:16 -0400

    wip

--- src/ZenToWooCommand.php
+++ src/ZenToWooCommand.php
@@ -51,7 +51,7 @@ class ZenToWooCommand extends WP_CLI_Command {
 			if(isset($record['image']) && $record['image']) {
 				$attachment_id = media_sideload_image('https://dev.donhume.com/wp-content/uploads/zentowoo/images/' . $record['image'], $product->get_id(), $record['name'], 'id');
 				if(is_wp_error($attachment_id)) {
-					WP_CLI::error( $attachment_id->get_error_message() . ': Unable to sideload image: ' . $record['image'] );
+					WP_CLI::error( $attachment_id->get_error_message() . ': Unable to sideload image: ' . $record['image'], false );
 				} else {
 					WP_CLI::log( 'Imported image: ' . $attachment_id );
 					$product->set_image_id(
@@ -66,5 +66,55 @@ class ZenToWooCommand extends WP_CLI_Command {
 		}
 
 		WP_CLI::success( 'Imported ' . count($products) . ' products' );
+	}
+
+	function categories( $args, $assoc_args )
+	{
+		if(!isset($args[0])) {
+			WP_CLI::error( 'No file path provided' );
+			return;
+		}
+
+		//load the CSV document from a file path
+		$csv = Reader::from($args[0], 'r');
+		$csv->setHeaderOffset(0);
+
+		$categories = [];
+		$subcategories = [];
+		$records = $csv->getRecords(['categories_id', 'parent_id', 'categories_name', 'categories_description', 'sort_order', 'categories_status']);
+		foreach($records as $record) {
+
+			if($record['parent_id'] == 0) {
+				$subcategories[$record['categories_id']] = $record;
+			} else {
+				$categories[$record['parent_id']] = $record;
+			}
+		}
+
+		$categories_id_lookup = [];
+		foreach($categories as $category) {
+			$result = wp_insert_term( $category['categories_name'], 'product_cat');
+
+			if(is_wp_error($result)) {
+				WP_CLI::error( $result->get_error_message() );
+			} else {
+				$categories_id_lookup[$category['categories_id']] = $result['term_id'];
+				WP_CLI::log( 'Imported category: ' . $result['term_id'] );
+			}
+		}
+
+		foreach($subcategories as $subcategory) {
+
+			$result = wp_insert_term( $category['categories_name'], 'product_cat', [
+				'parent' => $categories_id_lookup[$subcategory['categories_id']]
+			]);
+
+			if(is_wp_error($result)) {
+				WP_CLI::error( $result->get_error_message() );
+			} else {
+				$categories_id_lookup[$category['categories_id']] = $result['term_id'];
+				WP_CLI::log( 'Imported subcategory: ' . $result['term_id'] );
+			}
+		}
 	}
 }
