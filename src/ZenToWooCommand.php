68a8c3a21c51ac1a4ef97ec9c134bb58c5b6a9fa
Kyle B. Johnson <kyle.johnson@hey.com>
2026-04-29 16:07:35 -0400

    wip

--- src/ZenToWooCommand.php
+++ src/ZenToWooCommand.php
@@ -42,10 +42,10 @@ class ZenToWooCommand extends WP_CLI_Command {
 			return;
 		}
 
-		if(!isset($args[3])) WP_CLI::error( 'No attribute JSON file specified' );
-		$attribute_import = file_get_contents($args[3]);
-		$attribute_import_data = json_decode($attribute_import, true);
-		if(is_null($attribute_import_data)) WP_CLI::error( 'Unable to parse attribute JSON file' );
+		if(!isset($args[3])) WP_CLI::error( 'No product options JSON file specified' );
+		$options_import = file_get_contents($args[3]);
+		$options_import_data = json_decode($options_import, true);
+		if(is_null($options_import_data)) WP_CLI::error( 'Unable to parse product options JSON file' );
 
 		// CATEGORIES
 
@@ -172,110 +172,63 @@ class ZenToWooCommand extends WP_CLI_Command {
 //			WP_CLI::success( 'Mapped product: ' . $product_id . ' to ' . count($category_ids) . ' categories' );
 //		}
 
+		// OPTIONS
 
-		// ATTRIBUTES
+		$product_field_id_lookup = [];
+		foreach($options_import_data as $data) {
 
-		foreach($attribute_import_data as $data) {
+			$product_id = $product_id_lookup[$data['product_id']];
 
-			if(!isset($product_id_lookup[$data['id']])) {
-				WP_CLI::error( 'Product ID not found: ' . $data['id'], false );
-				continue;
-			}
+			if(isset($product_field_id_lookup[$data['product_id']][$data['product_id']])) {
 
-			$product_id = $product_id_lookup[$data['id']];
+				$field_id = wp_insert_post([
+					'post_type'   => 'af_pao_fields',
+					'post_status' => 'publish',
+					'post_title'  => $data['option_name'],
+					'post_parent' => $product_id,
+				]);
 
-			$attributes_data = $data['attributes'];
-
-			$option_price_modifier_lookup = [];
-
-			if( sizeof($attributes_data) > 0 ){
-
-				$attributes = array(); // Initializing
-
-				// Loop through defined attribute data
-				foreach( $attributes_data as $key => $attribute_array ) {
-					if( isset($attribute_array['name']) && isset($attribute_array['options']) ){
-						// Clean attribute name to get the taxonomy
-						$taxonomy = 'pa_' . wc_sanitize_taxonomy_name( $attribute_array['name'] );
-
-						if(!taxonomy_exists($taxonomy)) {
-							wc_create_attribute([
-								'name' => $attribute_array['name'],
-								'slug' => wc_sanitize_taxonomy_name( $attribute_array['name'] ),
-							]);
-							WP_CLI::log( 'Created taxonomy: ' . $taxonomy );
-						}
-
-						$option_term_ids = array(); // Initializing
-
-						// Loop through defined attribute data options (terms values)
-						foreach( $attribute_array['options'] as $option ){
-							if( term_exists( $option['name'], $taxonomy ) ){
-								// Save the possible option value for the attribute which will be used for variation later
-								wp_set_object_terms( $product_id, $option['name'], $taxonomy, true );
-
-								// Get the term ID
-								$term = get_term_by( 'name', $option['name'], $taxonomy );
-								$option_price_modifier_lookup[$term->term_id] = $option['price_modifier'];
-								$option_term_ids[] = $term->term_id;
-
-							} else {
-								$result = wp_insert_term( $option['name'], $taxonomy );
-
-								if(is_wp_error($result)) {
-									WP_CLI::error( 'Failed to insert attribute term: ' . $taxonomy . '. ' . $result->get_error_message(), false );
-								} else {
-									$option_term_ids[] = $result['term_id'];
-									$option_price_modifier_lookup[$result['term_id']] = $option['price_modifier'];
-								}
-
-							}
-						}
-					}
-					// Loop through defined attribute data
-					$attributes[$taxonomy] = array(
-						'name'          => $taxonomy,
-						'value'         => $option_term_ids, // Need to be term IDs
-						'position'      => $key + 1,
-						'is_visible'    => $attribute_array['visible'],
-						'is_variation'  => $attribute_array['variation'],
-						'is_taxonomy'   => '1'
-					);
+				$field_type = [
+					'Dropdown' => 'drop_down',
+					'Text' => 'input_text',
+					'Radio' => 'radio',
+					'Checkbox' => 'check_boxes',
+					'File' => 'file_upload',
+					'Read Only' => '',
+					'button' => '',
+				][ $data['type'] ];
+				if(!$field_type) {
+					WP_CLI::error( 'Unknown field type: ' . $data['type'], false );
+					continue;
 				}
-				// Save the meta entry for product attributes
-				update_post_meta( $product_id, '_product_attributes', $attributes );
-				WP_CLI::success( 'Imported attributes for product: ' . $product_id );
+				update_post_meta( $field_id, 'af_addon_type_select', $field_type );
+
+				$field_title = sanitize_meta( '', $data['option_name'], '' );
+				update_post_meta( $field_id, 'af_addon_field_title', $field_title );
+
+				$product_field_id_lookup[$data['product_id']][$data['product_id']] = $field_id;
+
+				WP_CLI::log( 'Imported product field: ' . $data['option_name'] );
+			}
+			$field_id = $product_field_id_lookup[$data['product_id']][$data['product_id']];
+
+			$option_id = wp_insert_post([
+				'post_type'   => 'af_pao_options',
+				'post_status' => 'publish',
+				'post_title'  => $data['value'],
+				'post_parent' => $field_id,
+			]);
+
+			$option_name = sanitize_meta( '', $data['value'], '' );
+			update_post_meta( $option_id, 'af_addon_field_options_name', $option_name );
+
+			if($data['price_modifier']) {
+				$option_price = sanitize_meta( '', $data['price_modifier'], '' );
+				update_post_meta( $option_id, 'af_addon_field_options_price', $option_price );
+				update_post_meta( $option_id, 'af_addon_field_options_price_type', 'af_addon_flat_fee' );
 			}
 
-			// PRODUCT VARIATIONS
-			$product = wc_get_product( $product_id );
-			$data_store = $product->get_data_store();
-			if ( ! is_callable( array( $data_store, 'create_all_product_variations' ) ) ) {
-				WP_CLI::error( 'Product variations not supported for product ID ' . $product_id, false );
-				continue;
-			}
-
-			WP_CLI::log( 'Generating variations for product: ' . $product_id );
-			$variations_count = $data_store->create_all_product_variations( $product );
-			WP_CLI::log( 'Sorting variations for product: ' . $product_id );
-			$data_store->sort_all_product_variations( $product->get_id() );
-			WP_CLI::success( 'Generated ' . $variations_count . ' variations for product: ' . $product_id );
-
-			$variations = $product->get_available_variations();
-			if( $variations ) {
-				foreach( $variations as $variation ) {
-					// a WC_Product_Variation object
-					if(isset($option_price_modifier_lookup[$variation->get_attribute( 'pa_price_modifier' )])) {
-						$variation->set_price( $variation->get_price() + $option_price_modifier_lookup[$variation->get_attribute( 'pa_price_modifier' )] );
-					} else {
-						$variation->set_price( $variation->get_price() );
-					}
-
-					WP_CLI::log( 'Set variation price for product: ' . $product_id . ' to ' . $variation->get_price() );
-				}
-			}
+			WP_CLI::log( 'Imported product option: ' . $data['value'] );
 		}
-
 	}
-
 }
